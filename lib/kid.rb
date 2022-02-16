ed8e419934b3cbf2dcb0d12780ba2d23ca2ab6a0
Vikalp Malhotra <kimboho@gmail.com>
2022-02-15 23:25:22 -0500

    commit tree e85b169dda990d2da7bea78f6878b29116984a2d

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,19 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+require 'fancy_dance.rb'
+
+class Kid
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+    include Dance
+    attr_accessor :name
+    extend MetaDancing
+
+
+    def initialize(name)
+        @name = name
+
+    end
+
+end
